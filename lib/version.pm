9d8aa41ed76f0530cd4b0cd63761fc507be0c97e
John Peacock <john.peacock@havurah-software.org>
2005-09-14 15:22:04 +0000

    Extract POD into seperate file and re-add version.pm

---
+++ lib/version.pm
@@ -0,0 +1,35 @@
+#!perl -w
+package version;
+
+use 5.005_03;
+use strict;
+
+require Exporter;
+use vars qw(@ISA $VERSION $CLASS @EXPORT);
+
+@ISA = qw(Exporter);
+
+@EXPORT = qw(qv);
+
+$VERSION = "0.48"; 
+
+$CLASS = 'version';
+
+eval { require version::vxs; };
+
+if ( $@ ) # don't have the XS version installed
+{
+    eval { require version::vpp };
+    die "$@" if ( $@ );
+    push @ISA, "version::PP";
+    *qv = \&version::vpp::qv;
+}
+else # use XS module
+{
+    push @ISA, "version::vxs";
+    *qv = \&version::vxs::qv;
+}
+
+# Preloaded methods go here.
+
+1;
