3f31a6e89ef037348573ba1ee92708748dda69ba
John Peacock <john.peacock@havurah-software.org>
2011-08-21 06:38:16 -0400

    Prevent DIE handlers in user code from tripping up loading version. Don't know how this hasn't shown up until now.  Resolves:
      https://rt.cpan.org/Ticket/Display.html?id=70260

--- lib/version.pm
+++ lib/version.pm
@@ -6,7 +6,7 @@ use strict;
 
 use vars qw(@ISA $VERSION $CLASS $STRICT $LAX *declare *qv);
 
-$VERSION = 0.93;
+$VERSION = 0.94;
 
 $CLASS = 'version';
 
@@ -114,38 +114,41 @@ $LAX =
 
 #--------------------------------------------------------------------------#
 
-eval "use version::vxs $VERSION";
-if ( $@ ) { # don't have the XS version installed
-    eval "use version::vpp $VERSION"; # don't tempt fate
-    die "$@" if ( $@ );
-    push @ISA, "version::vpp";
-    local $^W;
-    *version::qv = \&version::vpp::qv;
-    *version::declare = \&version::vpp::declare;
-    *version::_VERSION = \&version::vpp::_VERSION;
-    if ($] >= 5.009000) {
-	no strict 'refs';
-	*version::stringify = \&version::vpp::stringify;
-	*{'version::(""'} = \&version::vpp::stringify;
-	*version::new = \&version::vpp::new;
-	*version::parse = \&version::vpp::parse;
+{
+    local $SIG{'__DIE__'};
+    eval "use version::vxs $VERSION";
+    if ( $@ ) { # don't have the XS version installed
+	eval "use version::vpp $VERSION"; # don't tempt fate
+	die "$@" if ( $@ );
+	push @ISA, "version::vpp";
+	local $^W;
+	*version::qv = \&version::vpp::qv;
+	*version::declare = \&version::vpp::declare;
+	*version::_VERSION = \&version::vpp::_VERSION;
+	if ($] >= 5.009000) {
+	    no strict 'refs';
+	    *version::stringify = \&version::vpp::stringify;
+	    *{'version::(""'} = \&version::vpp::stringify;
+	    *version::new = \&version::vpp::new;
+	    *version::parse = \&version::vpp::parse;
+	}
     }
-}
-else { # use XS module
-    push @ISA, "version::vxs";
-    local $^W;
-    *version::declare = \&version::vxs::declare;
-    *version::qv = \&version::vxs::qv;
-    *version::_VERSION = \&version::vxs::_VERSION;
-    *version::vcmp = \&version::vxs::VCMP;
-    if ($] >= 5.009000) {
-	no strict 'refs';
-	*version::stringify = \&version::vxs::stringify;
-	*{'version::(""'} = \&version::vxs::stringify;
-	*version::new = \&version::vxs::new;
-	*version::parse = \&version::vxs::parse;
-    }
+    else { # use XS module
+	push @ISA, "version::vxs";
+	local $^W;
+	*version::declare = \&version::vxs::declare;
+	*version::qv = \&version::vxs::qv;
+	*version::_VERSION = \&version::vxs::_VERSION;
+	*version::vcmp = \&version::vxs::VCMP;
+	if ($] >= 5.009000) {
+	    no strict 'refs';
+	    *version::stringify = \&version::vxs::stringify;
+	    *{'version::(""'} = \&version::vxs::stringify;
+	    *version::new = \&version::vxs::new;
+	    *version::parse = \&version::vxs::parse;
+	}
 
+    }
 }
 
 # Preloaded methods go here.
--- vutil/lib/version/vxs.pm
+++ vutil/lib/version/vxs.pm
@@ -6,7 +6,7 @@ use strict;
 
 use vars qw(@ISA $VERSION $CLASS );
 
-$VERSION = 0.93;
+$VERSION = 0.94;
 
 $CLASS = 'version::vxs';
 
