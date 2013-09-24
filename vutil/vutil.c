d8e49462d50b992312bbd2cd321b84c661bf2eba
John Peacock <john.peacock@havurah-software.org>
2013-09-23 22:06:37 -0400

    Handle integer-only versions that overflow either UV or IV

--- vutil/vutil.c
+++ vutil/vutil.c
@@ -591,6 +591,15 @@ Perl_upg_version(pTHX_ SV *ver, bool qv)
 	qv = TRUE;
     }
 #endif
+    else if ( (SvUOK(ver) && SvUVX(ver) > VERSION_MAX)
+	   || (SvIOK(ver) && SvIVX(ver) > VERSION_MAX) ) {
+	STRLEN len;
+	char tbuf[64];
+	len = my_snprintf(tbuf, sizeof(tbuf), "%d", VERSION_MAX);
+	version = savepvn(tbuf, len);
+	Perl_ck_warner(aTHX_ packWARN(WARN_OVERFLOW),
+		       "Integer overflow in version %d",VERSION_MAX);
+    }
     else /* must be a string or something like a string */
     {
 	STRLEN len;
