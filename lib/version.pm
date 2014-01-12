008f321941224fc848d68674bafea8fa4c649ba3
John Peacock <john.peacock@havurah-software.org>
2014-01-12 10:09:12 -0500

    $VERSION++ for CPAN release.

--- lib/version.pm
+++ lib/version.pm
@@ -6,7 +6,7 @@ use strict;
 
 use vars qw(@ISA $VERSION $CLASS $STRICT $LAX *declare *qv);
 
-$VERSION = 0.9906;
+$VERSION = 0.9907;
 $CLASS = 'version';
 
 # !!!!Delete this next block completely when adding to Perl core!!!!
